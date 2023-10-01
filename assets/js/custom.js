eab45d873daa64b98ca266a6454cecb2e7151d4d
Muhammadhamd <muhammadhamdali572@gmail.com>
2023-10-02 04:05:02 +0500

    fafga

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,100 +1,174 @@
 $(document).ready(function () {
-    $("#owl1").owlCarousel({
-        margin: 10,
-        loop: true,
-        nav: true,
-        navText: ["<img src='./assets/images/icons/left-arrow.png'>","<img src='./assets/images/icons/right-arrow.png'>"],
+  $("#owl1").owlCarousel({
+      margin: 10,
+      loop: true,
+      nav: true,
+      navText: ["<img src='./assets/images/icons/left-arrow.png'>","<img src='./assets/images/icons/right-arrow.png'>"],
 
-        responsive: {
-            0: {
-                items: 1,
-                nav: false
-            },
-            600: {
-                items: 2,
-                nav: true
-            },
-            1000: {
-                items: 1,
-                nav: true
-            }
+      responsive: {
+          0: {
+              items: 1,
+              nav: false
+          },
+          600: {
+              items: 2,
+              nav: true
+          },
+          1000: {
+              items: 1,
+              nav: true
+          }
+      }
+  });
+  $('#owl2').owlCarousel({
+      loop: true,
+      margin: 10,
+      nav: true,
+      center: true,
+      nav: true,
+      navText: ["<img src='./assets/images/home/awesome-arrow-left.png'>","<img src='./assets/images/home/awesome-arrow-right.png'>"],
+      responsive: {
+          0: {
+              items: 1
+          },
+          600: {
+              items: 1
+          },
+          1000: {
+              items: 3
+          }
+      }
+  })
+  $('#owl3').owlCarousel({
+      loop: true,
+      margin: 10,
+      nav: false,
+      responsive: {
+          0: {
+              items: 1
+          },
+          600: {
+              items: 1
+          },
+          1000: {
+              items: 4
+          }
+      }
+  })
+
+
+  $("#team").owlCarousel({
+      margin: 10,
+      loop: true,
+      responsive: {
+        0:{
+          items:1,
+          nav: false
+        },
+        600:{
+          items:1,
+          nav: false
+        },
+        1000:{
+          items:4,
+          nav: false
         }
+      }
     });
-    $('#owl2').owlCarousel({
-        loop: true,
-        margin: 10,
-        nav: true,
-        center: true,
-        nav: true,
-        navText: ["<img src='./assets/images/home/awesome-arrow-left.png'>","<img src='./assets/images/home/awesome-arrow-right.png'>"],
-        responsive: {
-            0: {
-                items: 1
-            },
-            600: {
-                items: 1
-            },
-            1000: {
-                items: 3
-            }
+  $("#services_owl").owlCarousel({
+      margin: 10,
+      loop: true,
+      responsive: {
+        0:{
+          items:1,
+          nav: false
+        },
+        600:{
+          items:1,
+          nav: false
+        },
+        1000:{
+          items:4,
+          nav: false
         }
-    })
-    $('#owl3').owlCarousel({
-        loop: true,
-        margin: 10,
-        nav: false,
-        responsive: {
-            0: {
-                items: 1
-            },
-            600: {
-                items: 1
-            },
-            1000: {
-                items: 4
-            }
-        }
-    })
-    $("#team").owlCarousel({
-        margin: 10,
-        loop: true,
-        responsive: {
-          0:{
-            items:1,
-            nav: false
-          },
-          600:{
-            items:1,
-            nav: false
-          },
-          1000:{
-            items:4,
-            nav: false
-          }
-        }
-      });
-    $("#services_owl").owlCarousel({
-        margin: 10,
-        loop: true,
-        responsive: {
-          0:{
-            items:1,
-            nav: false
-          },
-          600:{
-            items:1,
-            nav: false
-          },
-          1000:{
-            items:4,
-            nav: false
-          }
-        }
-      });
+      }
+    });
 
-    AOS.init();
-    // End Ready fuction
-    $("#thumbnailslider").owlCarousel({
+  AOS.init();
+  // End Ready fuction
+  $("#thumbnailslider").owlCarousel({
+    margin: 10,
+    loop: true,
+    responsive: {
+      0:{
+        items:1,
+        nav: false
+      },
+      600:{
+        items:1,
+        nav: false
+      },
+      1000:{
+        items:1,
+        nav: false
+      }
+    }
+  });
+  //image overlay loader
+
+
+  $("#thumbnailslider1").owlCarousel({
+    margin: 10,
+    loop: true,
+    responsive: {
+      0:{
+        items:1,
+        nav: false
+      },
+      600:{
+        items:1,
+        nav: false
+      },
+      1000:{
+        items:3,
+        nav: false
+      }
+    }
+  });
+
+
+
+    $("#toggle").click(function(){
+      $(".toggle-content").toggle();
+    });
+    $("#toggle1").click(function(){
+      $(".toggle-content1").toggle();
+    });
+  
+    $("#toggle2").click(function(){
+      $(".toggle-content2").toggle();
+    });
+    $("#toggle3").click(function(){
+      $(".toggle-content3").toggle();
+    });
+    $("#toggle4").click(function(){
+      $(".toggle-content4").toggle();
+    });
+    /*Drop down menu*/
+
+    // $('.menu-dropdown').hover(function () {
+    //     $(".slide-show").show();
+
+    //   });
+    //   $('.menu-dropdown').mouseout(function () {
+    //     $(".slide-show").hide();
+
+    //   });
+    
+
+
+
+    $("#sellslider").owlCarousel({
       margin: 10,
       loop: true,
       responsive: {
@@ -112,55 +186,10 @@ $(document).ready(function () {
         }
       }
     });
-    $("#thumbnailslider1").owlCarousel({
-      margin: 10,
-      loop: true,
-      responsive: {
-        0:{
-          items:1,
-          nav: false
-        },
-        600:{
-          items:1,
-          nav: false
-        },
-        1000:{
-          items:3,
-          nav: false
-        }
-      }
-    });
 
-      $("#toggle").click(function(){
-        $(".toggle-content").toggle();
-      });
-      $("#toggle1").click(function(){
-        $(".toggle-content1").toggle();
+
       });
     
-      $("#toggle2").click(function(){
-        $(".toggle-content2").toggle();
-      });
-      $("#toggle3").click(function(){
-        $(".toggle-content3").toggle();
-      });
-      $("#toggle4").click(function(){
-        $(".toggle-content4").toggle();
-      });
-      /*Drop down menu*/
- 
-      // $('.menu-dropdown').hover(function () {
-      //     $(".slide-show").show();
-
-      //   });
-      //   $('.menu-dropdown').mouseout(function () {
-      //     $(".slide-show").hide();
-
-      //   });
-
-
-        });
-      
 
 
 
@@ -172,13 +201,13 @@ $('.tab-content:first').css("background-color");
 
 // Click function
 $('#tabs-nav li').click(function(){
-  $('#tabs-nav li').removeClass('active');
-  $(this).addClass('active');
-  $('.tab-content').hide();
-  
-  var activeTab = $(this).find('a').attr('href');
-  $(activeTab).fadeIn();
-  return false;
+$('#tabs-nav li').removeClass('active');
+$(this).addClass('active');
+$('.tab-content').hide();
+
+var activeTab = $(this).find('a').attr('href');
+$(activeTab).fadeIn();
+return false;
 });
 
 const toggleBtn = document.querySelector(".toggle_btn");
@@ -186,7 +215,20 @@ const toggleBtnIcon = document.querySelector(".toggle_btn i");
 const dropDownMenu = document.querySelector(".drop_down");
 
 toggleBtn.onclick = function () {
-  dropDownMenu.classList.toggle("open");
-  const isOpen = dropDownMenu.classList.contains("open");
-  toggleBtnIcon.className = isOpen ? "bi bi-x" : "bi bi-list";
+dropDownMenu.classList.toggle("open");
+const isOpen = dropDownMenu.classList.contains("open");
+toggleBtnIcon.className = isOpen ? "bi bi-x" : "bi bi-list";
 };
+
+
+setTimeout(function() {
+  $('.popup-overlay').show();
+}, 5000);
+
+$('.cross').click(function(){
+$('.popup-overlay').fadeOut(1500);
+});
+
+
+
+
